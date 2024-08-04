0bdc692a9602a29f10a658e6355b248efd1d5d76
bhavyansh <bhavyansh2018@gmail.com>
2024-08-04 12:11:05 +0530

    updating MainViewModel and MainActivity

--- app/src/main/java/com/example/onboarding_screen_custom_circular_progress/MainActivity.kt
+++ app/src/main/java/com/example/onboarding_screen_custom_circular_progress/MainActivity.kt
@@ -8,12 +8,17 @@ import androidx.activity.viewModels
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.onboarding_screen_custom_circular_progress.presentation.navigation.navGraph.NavGraph
 import com.example.onboarding_screen_custom_circular_progress.presentation.viewModel.MainViewModel
 import com.example.onboarding_screen_custom_circular_progress.ui.theme.OnboardingScreenCustomCircularProgressTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -23,20 +28,27 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        installSplashScreen().apply {
-            setKeepOnScreenCondition{
-                viewModel.splashCondition
+        // Initialize splash screen
+        val splashScreen = installSplashScreen()
+
+        // Use coroutine scope to handle the splash condition
+        val splashScope = CoroutineScope(Dispatchers.Main)
+        splashScope.launch {
+            viewModel.splashCondition.collect { splashCondition ->
+                splashScreen.setKeepOnScreenCondition { splashCondition }
             }
         }
 
         enableEdgeToEdge()
         setContent {
             OnboardingScreenCustomCircularProgressTheme {
+                val startDestination by viewModel.startDestination.collectAsState()
                 Box(
                     modifier = Modifier.background(color = MaterialTheme.colorScheme.background)
-                ){
-                    val startDestination = viewModel.startDestination
-                    NavGraph(startDestination = startDestination)
+                ) {
+                    if (startDestination != null) {
+                        NavGraph(startDestination = startDestination!!)
+                    }
                 }
             }
         }
